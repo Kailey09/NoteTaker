bcb9d42b20294f70afdc1e4be02fe18eeba363e7
Kailey09 <kcoop1323@gmail.com>
2022-06-05 00:14:43 -0400

    add heroku

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/", function(req,res) {
 });
 
 app.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "./public/idex.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 app.listen(PORT, function() {
