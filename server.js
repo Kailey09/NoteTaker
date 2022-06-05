23a83216862ca7738cdaf21bd7b334bf83d46fd4
Kailey09 <kcoop1323@gmail.com>
2022-06-04 23:35:55 -0400

    added get route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const fs = require("fs");
-const notes = require("./db/db.json");
 const path = require("path");
 const util = require("util");
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const app = express();
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("./public"));
+
+
